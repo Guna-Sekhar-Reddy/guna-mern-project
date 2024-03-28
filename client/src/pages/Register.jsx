ddca3f529d87dd9dfc0a59618c6fa2e65bca596a
guna <gunasekharreddymorusu999@gmail.com>
2024-03-28 22:22:21 +0530

    fix front end

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -25,16 +25,11 @@ const Register = () => {
       <Form method="post" className="form">
         <Logo />
         <h4>Register</h4>
-        <FormRow type="text" name="name" defaultValue={"Guna"} />
-        <FormRow
-          type="text"
-          name="lastName"
-          labelText="last name"
-          defaultValue={"morusu"}
-        />
-        <FormRow type="text" name="location" defaultValue={"earth"} />
+        <FormRow type="text" name="name" />
+        <FormRow type="text" name="lastName" labelText="last name" />
+        <FormRow type="text" name="location" />
         <FormRow type="email" name="email" />
-        <FormRow type="password" name="password" defaultValue={"abcdefgh"} />
+        <FormRow type="password" name="password" />
         <SubmitBtn />
         <p>
           Already a member?
